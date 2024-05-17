55b467e7d238ad7fe83fc929b95b66688a7fdf96
Matt Rundle <m.n.rundle@gmail.com>
2024-05-16 23:49:55 -0500

    completed: move semantics

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -3,21 +3,41 @@
 // Execute `rustlings hint move_semantics1` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
-
     let vec1 = fill_vec(vec0);
-
     assert_eq!(vec1, vec![22, 44, 66, 88]);
+
+    //let vec0 = vec![22, 44, 66];
+    //let vec1 = fill_vec_from_ref(&vec0);
+    //assert_eq!(vec1, vec![22, 44, 66, 88]);
+
+    //let mut vec0 = vec![22, 44, 66];
+    //fill_vec_mut(&mut vec0);
+    //assert_eq!(vec0, vec![22, 44, 66, 88]);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+/* pass by reference and then create copy */
+fn fill_vec_from_ref(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec2: Vec<i32> = [].to_vec();
+    for i in vec.iter() {
+        vec2.push(*i);
+    }
+    vec2.push(88);
+    return vec2;
+}
 
+/* mutable reference, modify original */
+fn fill_vec_mut(vec: &mut Vec<i32>) {
     vec.push(88);
+}
 
+/*
+ * the actual solution; pass by copy on the stack?
+ */
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+    vec.push(88);
     vec
 }
--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -6,19 +6,16 @@
 // Execute `rustlings hint move_semantics3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn main() {
+    /* pass by mutable value; vec1 now owns the data */
     let vec0 = vec![22, 44, 66];
-
     let vec1 = fill_vec(vec0);
-
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+/* this takes ownership of vec; vec0 can't be used after */
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
-
     vec
 }
